04767db73c8b3c41030884676a0e4447fa7440d7
hmeissner <hugomeissner@icloud.com>
2017-11-08 21:51:40 +0100

    initial

---
+++ plugins/gatsby-source-graphcms/src/gatsby-node.js
@@ -0,0 +1,63 @@
+import { GraphQLClient } from "graphql-request"
+import R from "ramda"
+import crypto from "crypto"
+import { extractTypeName } from "./util"
+
+const SOURCE_NAME = `GraphCMS`
+
+const DEBUG_MODE = false
+
+exports.sourceNodes = async (
+  { boundActionCreators, reporter },
+  { endpoint, token, query }
+) => {
+  const { createNode } = boundActionCreators
+
+  const clientOptions = {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+  }
+
+  const client = new GraphQLClient(endpoint, clientOptions)
+
+  if (query) {
+    const userQueryResult = await client.request(query)
+    if (DEBUG_MODE) {
+      const jsonUserQueryResult = JSON.stringify(userQueryResult, undefined, 2)
+      console.log(`\ngatsby-source-graphcms: GraphQL query results: ${jsonUserQueryResult}`)
+    }
+    R.forEachObjIndexed(createNodes(createNode, reporter), userQueryResult)
+  } else {
+    reporter.panic(`gatsby-source-graphcms: you need to provide a GraphQL query in the plugin 'query' parameter`)
+  }
+}
+
+const createNodes = (createNode, reporter) => 
+  (value, key) =>
+    R.forEach(queryResultNode => {
+      const { id, ...fields } = queryResultNode
+      const jsonNode = JSON.stringify(queryResultNode)
+      const gatsbyNode = {
+          id,
+          ...fields,
+          parent: `${SOURCE_NAME}_${key}`,
+          children: [],
+          internal: {
+            type: extractTypeName(key),
+            content: jsonNode,
+            contentDigest: crypto.createHash(`md5`).update(jsonNode).digest(`hex`),
+          },
+        }
+      if (DEBUG_MODE) {
+        const prettyJsonNode = JSON.stringify(queryResultNode, null, 2)
+        const jsonFields = JSON.stringify(fields, null, 2)
+        const jsonGatsbyNode = JSON.stringify(gatsbyNode, null, 2)
+        reporter.info(`    processing node: ${prettyJsonNode}`)
+        reporter.info(`    node id ${id}`)
+        reporter.info(`    node fields: ${jsonFields}`)
+        reporter.info(`    gatsby node: ${jsonGatsbyNode}`)
+      }
+
+      createNode(gatsbyNode)
+    }, value)
